b1797d2506b4af8a0196d2f6beea6ea958ecf426
Karlo O'Brien <karlobrien@gmail.com>
2026-06-29 21:50:50 +0100

    feat: add oh-my-zsh + p10k install script

---
+++ .chezmoiscripts/run_once_02_ohmyzsh.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Install Oh My Zsh (unattended, don't switch shell or start zsh after install)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install powerlevel10k theme
+P10K_DIR="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+if [ ! -d "$P10K_DIR" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$P10K_DIR"
+fi
